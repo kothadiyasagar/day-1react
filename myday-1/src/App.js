6f08f03ba92a34ba62b8263b60d5f8cf7b954949
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-04-07 22:53:05 +0530

    done

--- myday-1/src/App.js
+++ myday-1/src/App.js
@@ -8,8 +8,9 @@ function App() {
   return (
     <>
      <Static/>
-     <div className='a'><Join/>
-     <Setting/>
+     <div className='a'>
+       <Join h1="JOIN US" h2="LOGIN" h3="SEARCH" h4="HOME"/>
+     <Setting h1="SETTINGS" h2="CONTACT US" h3="HELP" h4="DOWNLOAD"/>
      </div>
       
     </>
--- myday-1/src/settings.jsx
+++ myday-1/src/settings.jsx
@@ -1,18 +1,18 @@
 
-function Setting (){
+function Setting (props){
     return(
         <div>
             <div>
-            <button style={{color:"white",background:"gray",marginBottom:"10px",marginLeft:"70px",width:"170px",borderRadius:"25px",padding:"10px",border:"1px white"}}>SETTINGS</button>
+            <button style={{color:"white",background:"gray",marginBottom:"10px",marginLeft:"70px",width:"170px",borderRadius:"25px",padding:"10px",border:"1px white"}}>{props.h1}</button>
             </div>
             <div>
-            <button style={{color:"white",background:"red",marginBottom:"10px",marginLeft:"70px",width:"170px",borderRadius:"25px",padding:"10px",border:"1px white"}}>CONTACT US</button>
+            <button style={{color:"white",background:"red",marginBottom:"10px",marginLeft:"70px",width:"170px",borderRadius:"25px",padding:"10px",border:"1px white"}}>{props.h2}</button>
             </div>
             <div>
-            <button style={{color:"white",background:"#6A5ACD",marginBottom:"10px",marginLeft:"70px",width:"170px",borderRadius:"25px",padding:"10px",border:"1px white"}}>SEARCH</button>
+            <button style={{color:"white",background:"#6A5ACD",marginBottom:"10px",marginLeft:"70px",width:"170px",borderRadius:"25px",padding:"10px",border:"1px white"}}>{props.h3}</button>
             </div>
             <div>
-            <button style={{color:"white",background:"#B8860B",marginBottom:"10px",marginLeft:"70px",width:"170px",borderRadius:"25px",padding:"10px",border:"1px white"}}>HOME</button>
+            <button style={{color:"white",background:"#B8860B",marginBottom:"10px",marginLeft:"70px",width:"170px",borderRadius:"25px",padding:"10px",border:"1px white"}}>{props.h4}</button>
             </div>
            
         </div>
